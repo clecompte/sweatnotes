da88a6b924fc31cc6d7ec833973aa5e2adc65909
Chris LeCompte <me@clecompte.com>
2018-03-31 12:34:50 -0400

    Add authentication to client

---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>
+        Sweat Notes
+      </h1>
+      Track fitness routines.
+    </div>
+  );
+};
+
+export default Landing;
